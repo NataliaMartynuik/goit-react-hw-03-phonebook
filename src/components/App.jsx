76114b681f932a80be2043abd5cbd97599cf10ce
NataliaMartynuik <98057592+NataliaMartynuik@users.noreply.github.com>
2022-10-31 22:27:29 +0200

    3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,17 +6,18 @@ import { Filter } from './Filter/Filter';
 import { Container, Title, TitleContact } from './App.styled';
 
 
-
-
-
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
+      { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
+      { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
+      { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
+    ],
     filter: ''
   
   }
-  
-  componentDidUpdate(prevProps, prevState) {
+  componentDidUpdate(_, prevState) {
     if (this.state.contacts !== prevState.contacts) {
       localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
     }
@@ -24,10 +25,10 @@ export class App extends Component {
 
   componentDidMount() {
     const contacts = localStorage.getItem('contacts')
-    const parsedContacts = JSON.parse(contacts)
-
-    this.setState({ contacts: parsedContacts})
+    if (contacts) {const parsedContacts = JSON.parse(contacts)
+    this.setState({ contacts: parsedContacts})}
   }
+  
     
   addContact = ({ name, number }) => {
     const { contacts } = this.state;
@@ -59,8 +60,8 @@ export class App extends Component {
   getVisibleContacts = () => {
      const { contacts, filter } = this.state
      const normalizedFilter = filter.toLowerCase()
-     return contacts.filter(contact =>
-     contact.name.toLowerCase().includes(normalizedFilter))
+     return contacts.filter(({ name })=>
+     name.toLowerCase().includes(normalizedFilter))
   }
 
 
